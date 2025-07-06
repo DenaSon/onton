bc7b1bd21d4c420aab9761eac4f5e458f0d453a2
Mohammad Assadi <denasoft@live.com>
2025-07-06 15:22:22 +0330

    Update Sandbox Iframe

--- resources/views/livewire/user-dashboard/feed/components/view-newsletter-modal.blade.php
+++ resources/views/livewire/user-dashboard/feed/components/view-newsletter-modal.blade.php
@@ -23,7 +23,7 @@
                     title="Preview"
                     src="{{ route('panel.newsletterView.html', ['id' => $newsletterId]) }}"
                     class="w-full h-[75vh] bg-white"
-                    sandbox
+                    sandbox="allow-same-origin"
                     @load="loaded = true"
                 ></iframe>
             </div>
