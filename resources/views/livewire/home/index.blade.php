464f5f55e9ab004204b613d6a95e1aa869fdecbc
Mohammad Assadi <denasoft@live.com>
2025-07-13 22:06:34 +0330

    Fix Home Bug

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -1,6 +1,25 @@
 <div>
 
     @push('meta')
+        <script type="application/ld+json">
+            {!! json_encode([
+                '@context' => 'https://schema.org',
+                '@type' => 'WebSite',
+                'name' => 'Byblos',
+                'url' => 'https://byblos.digital',
+                'description' => 'Follow top VC firms and get curated newsletters in your inbox with Byblos.',
+                'publisher' => [
+                    '@type' => 'Organization',
+                    'name' => 'Byblos',
+                    'url' => 'https://byblos.digital',
+                    'logo' => [
+                        '@type' => 'ImageObject',
+                        'url' => 'https://byblos.digital/static/img/byblos-logo.png',
+                    ]
+                ]
+            ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}
+        </script>
+
         <meta name="description"
               content="{{ 'Byblos | Follow top VC firms and get curated newsletters in your inbox' }}">
         <meta property="og:title" content="{{ $title ?? 'Byblos' }}">
@@ -9,27 +28,9 @@
         <meta property="og:image" content="{{ asset('static/img/byblos-hero.webp') }}">
         <meta property="og:url" content="{{ url()->current() }}">
         <meta name="twitter:card" content="summary_large_image">
-    @endpush
 
-        <script type="application/ld+json">
-            {
-                "@context": "https://schema.org",
-                "@type": "WebSite",
-                "name": "Byblos",
-                "url": "https://byblos.digital",
-                "description": "Follow top VC firms and get curated newsletters in your inbox with Byblos.",
-                "publisher": {
-                  "@type": "Organization",
-                  "name": "Byblos",
-                  "url": "https://byblos.digital",
-                  "logo": {
-                    "@type": "ImageObject",
-                    "url": "https://byblos.digital/static/img/byblos-logo.png"
-                  }
-                }
-              }
 
-        </script>
+
 
     @endpush
 
@@ -43,7 +44,7 @@
     <x-ui.home.pricing/>
 
 
-{{--    @include('components.ui.home.scroll-up-button')--}}
+        @include('components.ui.home.scroll-up-button')
 
 
 </div>
