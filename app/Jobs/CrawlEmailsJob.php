9ad990fb63cdc57aa1f279017347b07345114768
Mohammad Assadi <denasoft@live.com>
2025-12-11 23:20:13 +0330

    loockback-2000

--- app/Jobs/CrawlEmailsJob.php
+++ app/Jobs/CrawlEmailsJob.php
@@ -88,7 +88,7 @@ class CrawlEmailsJob implements ShouldQueue
             }
 
             $limit = 50;
-            $lookbackHours = 2500;
+            $lookbackHours = 2000;
 
             // Crawl inbox and spam folders separately
             $inboxEmails = $this->crawlFolder('INBOX', $whiteListEmails, $limit, $lookbackHours);
