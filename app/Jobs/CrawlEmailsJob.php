e58c933244097b266aab7d83dc4d6f94794570a1
Mohammad Assadi <denasoft@live.com>
2025-12-31 12:31:50 +0330

    handle

--- app/Jobs/CrawlEmailsJob.php
+++ app/Jobs/CrawlEmailsJob.php
@@ -125,17 +125,20 @@ class CrawlEmailsJob implements ShouldQueue
 
     public function failed(Throwable $exception): void
     {
-        Log::critical('[CrawlEmailsJob] Failed permanently!', [
-            'exception' => $exception->getMessage(),
-        ]);
+        Log::info('CrawlEmailJob retry');
 
-        User::notifyAdminsByRoleId(1, new UserSystemNotification(
-            subject: 'Email Crawling Failed',
-            title: 'Crawler Failure Alert',
-            message: 'The email crawling job failed permanently due to an error: ' . $exception->getMessage(),
-            actionUrl: url('core/log-viewer'),
-            actionText: 'View Logs',
-            footerText: 'This is an automated system alert from Byblos Crawler Bot.'
-        ));
+//        Log::critical('[CrawlEmailsJob] Failed permanently!', [
+//            'exception' => $exception->getMessage(),
+//        ]);
+//
+//        User::notifyAdminsByRoleId(1, new UserSystemNotification(
+//            subject: 'Email Crawling Failed',
+//            title: 'Crawler Failure Alert',
+//            message: 'The email crawling job failed permanently due to an error: ' . $exception->getMessage(),
+//            actionUrl: url('core/log-viewer'),
+//            actionText: 'View Logs',
+//            footerText: 'This is an automated system alert from Byblos Crawler Bot.'
+//        ));
+
     }
 }
